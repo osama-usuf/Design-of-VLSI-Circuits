1090fe7179d58e330078cbfc0deaeadc30b7d517
Osama Yousuf <35128083+osama-usuf@users.noreply.github.com>
2022-10-03 00:08:00 -0400

    progress on hw4 p1

--- hw4/p1/uart_tb.v
+++ hw4/p1/uart_tb.v
@@ -1,8 +1,12 @@
+`timescale 1us/1ps
+
 module uart_clk_gen_tb();
 
+    integer delay = 10;
     integer tb_cycle_number = 1;
     integer total_errors = 0;  // tracking total errors in assertions
-    integer i; // loop variable
+    real Clock_counter = 0;
+    real Sample_Clock_counter = 0; 
 
     // Inputs
     reg Sys_clock, reset;
@@ -15,7 +19,7 @@ module uart_clk_gen_tb();
     event tb_cycle_update;
     initial begin 
         forever @(tb_cycle_update) begin
-            tb_cycle_number = tb_cycle_number + 1;
+            tb_cycle_number <= tb_cycle_number + 1;
         end
     end
 
@@ -23,15 +27,15 @@ module uart_clk_gen_tb();
     event error;
     initial begin 
         forever @(error) begin
-            total_errors = total_errors + 1;
+            total_errors <= total_errors + 1;
         end
     end
 
     // Error Monitor
-    initial  begin
-        $display("\t\tTime, TB Cycle Number,\terror count"); 
-        $monitor("\t%d,\t%d", $time, tb_cycle_number, total_errors); 
-    end 
+    // initial  begin
+    //     $display("\t\tTime, TB Cycle Number,\terror count"); 
+    //     $monitor("\t%d,\t%d", $time, tb_cycle_number, total_errors); 
+    // end 
 
     // Watchdog - used for exiting and reports error monitor
     event watchdog;
@@ -41,11 +45,22 @@ module uart_clk_gen_tb();
             $finish;
         end
     end
-    // absolute timed watchdog
-    initial #5000 -> watchdog;
+    // absolute timed watchdog, terminate after 1 second of sim time (since we have to calculate the baud rate)
+    //initial #1000020 -> watchdog;
+
+    initial begin
+        forever @ (Clock) begin
+            Clock_counter <= Clock_counter + 1;
+        end
+    end
+
+    initial begin
+        forever @ (Sample_clock) begin
+            Sample_Clock_counter <= Sample_Clock_counter + 1;
+        end
+    end
 
     // Create the dvt
-
     uart_clk_gen dvt(// Outputs
                      .Clock(Clock),
                      .Sample_clock(Sample_clock),
@@ -55,24 +70,39 @@ module uart_clk_gen_tb();
                      .Sel_Baud_Rate(Sel_Baud_Rate));
     
     initial begin
-        Sys_clock = 1'b0;
-        forever #5 Sys_clock = ~Sys_clock;
+        Sys_clock <= 1'b0;
+        forever #0.0625 Sys_clock <= ~Sys_clock; // 8 MHz clock
     end
 
-    event reset_uart;
+    event case_1;
     initial begin 
-        forever @(reset_uart) begin
-            reset = 1'b0;
-            #10 reset = 1'b1;
+        forever @(case_1) begin
+            $display("\nSel_Baud_Rate\tClock\tSample_Clock");
             Sel_Baud_Rate = 3'b000;
+            // Reset the generator + counts
+            reset <= 1'b0;
+            #1 reset <= 1'b1;
+            Clock_counter <= 0;
+            Sample_Clock_counter <= 0;
+            // since we have to count the frequency / second, we ensure the sim runs for a complete second first
+            #(1000000+delay); 
+            $display("\t%b\t%d\t%f", Sel_Baud_Rate, Clock_counter / 2, Sample_Clock_counter / 2);
             -> tb_cycle_update;
         end
     end
 
-    event init_baud;
+    event case_2;
     initial begin 
-        forever @(init_baud) begin
-
+        forever @(case_2) begin
+            Sel_Baud_Rate <= Sel_Baud_Rate + 1;
+            // Reset the generator + frequency counters
+            reset <= 1'b0;
+            #1 reset <= 1'b1;
+            Clock_counter <= 0;
+            Sample_Clock_counter <= 0;
+            // since we have to count the frequency / second, we ensure the sim runs for a complete second first
+            #(1000000+delay); 
+            $display("\t%b\t%d\t%f", Sel_Baud_Rate, Clock_counter / 2, Sample_Clock_counter / 2);
             -> tb_cycle_update;
         end
     end
@@ -80,8 +110,9 @@ module uart_clk_gen_tb();
     // Test Case Loop
     always @ (*) begin
         case(tb_cycle_number)
-            1: -> reset_uart;
-            2: -> init_baud;
+            1: -> case_1;
+            2, 3, 4, 5, 6, 7, 8: -> case_2;
+            default: -> watchdog;
         endcase
     end
 
